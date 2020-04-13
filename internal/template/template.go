4911d04a20c8e3830a116de3e09ac823dc85c99e
Masashi Shinbara <shin1x1@gmail.com>
2020-04-13 12:57:52 +0900

    add conditional block by version to template (#2)

--- internal/template/template.go
+++ internal/template/template.go
@@ -2,6 +2,8 @@ package template
 
 import (
 	"bytes"
+	"github.com/Masterminds/semver/v3"
+	"io"
 	"io/ioutil"
 	"os"
 	"text/template"
@@ -9,10 +11,11 @@ import (
 
 type replacement struct {
 	Tag                 string
+	Version             string
 	TemplateDestination string
 }
 
-func ExecFile(src, dest, tag string) error {
+func ExecFile(src, dest, tag, version string) error {
 	data, err := ioutil.ReadFile(src)
 	if err != nil {
 		return err
@@ -23,21 +26,44 @@ func ExecFile(src, dest, tag string) error {
 		return err
 	}
 
-	t := template.Must(template.New("tmpl").Parse(string(data)))
-
-	r := replacement{Tag: tag}
-	return t.Execute(f, r)
+	return Exec(f, string(data), tag, version, dest)
 }
 
-func Exec(tmpl, tag, dest string) (string, error) {
-	t := template.Must(template.New("tmpl").Parse(tmpl))
-
-	r := replacement{Tag: tag, TemplateDestination: dest}
-
+func ExecString(tmpl, tag, version, dest string) (string, error) {
 	var b bytes.Buffer
-	if err := t.Execute(&b, r); err != nil {
-		return "", nil
+	if err := Exec(&b, tmpl, tag, version, dest); err != nil {
+		return "", err
 	}
 
 	return b.String(), nil
+}
+
+func Exec(writer io.Writer, tmpl, tag, version, dest string) error {
+	t := template.Must(template.New("tmpl").Funcs(funcMap()).Parse(tmpl))
+
+	r := replacement{Tag: tag, Version: version, TemplateDestination: dest}
+
+	return t.Execute(writer, r)
+}
+
+func funcMap() template.FuncMap {
+	return template.FuncMap{
+		"matchVersion": matchVersion,
+	}
+}
+
+func matchVersion(constraint, version string) (bool, error) {
+	c, err := semver.NewConstraint(constraint)
+	if err != nil {
+		return false, err
+	}
+
+	v, err := semver.NewVersion(version)
+	if err != nil {
+		return false, err
+	}
+
+	v1, _ := v.SetMetadata("")
+	v2, _ := v1.SetPrerelease("")
+	return c.Check(&v2), nil
 }
---
+++ internal/template/template_test.go
@@ -0,0 +1,63 @@
+package template
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestTemplate_Exec(t *testing.T) {
+	tmpl := `
+Tag: {{.Tag}}
+{{- if (matchVersion "^7.4" .Tag) }}
+^7.4
+{{- else }}
+other
+{{- end }}
+
+{{- if or (matchVersion "^7.4" .Tag) (eq "7" .Version) }}
+^7.4 or 7
+{{- else }}
+other
+{{- end }}
+`
+	params := []struct {
+		tag      string
+		version  string
+		expected string
+	}{
+		{
+			tag:      "7.4.0-cli",
+			version:  "7.4.0",
+			expected: "Tag: 7.4.0-cli\n^7.4\n^7.4 or 7",
+		},
+		{
+			tag:      "7.4-cli",
+			version:  "7.4",
+			expected: "Tag: 7.4-cli\n^7.4\n^7.4 or 7",
+		},
+		{
+			tag:      "7-cli",
+			version:  "7",
+			expected: "Tag: 7-cli\nother\n^7.4 or 7",
+		},
+		{
+			tag:      "7.3.0-cli",
+			version:  "7.3.0",
+			expected: "Tag: 7.3.0-cli\nother\nother",
+		},
+		{
+			tag:      "7.5.0-cli",
+			version:  "7.5.0",
+			expected: "Tag: 7.5.0-cli\n^7.4\n^7.4 or 7",
+		},
+	}
+
+	for k, v := range params {
+		result, err := ExecString(tmpl, v.tag, v.version, "")
+		if err != nil {
+			t.Errorf("%+v", err)
+		} else if strings.Trim(result, "\n") != v.expected {
+			t.Errorf("key:%d expected:\n%s\n\nactual:\n%s", k, v.expected, result)
+		}
+	}
+}
